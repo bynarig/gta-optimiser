3e6d664e535e2b17518c718082caf01ada363704
Yaroslav Shabelnyk <62596807+bynarig@users.noreply.github.com>
2025-06-10 21:34:29 +0200

    production config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enables static HTML export
 
-const nextConfig: NextConfig = {
-    output: 'export',
-  /* config options here */
+  // REQUIRED FOR GITHUB PAGES (PROJECT PAGES)
+  basePath: process.env.NODE_ENV === 'production' ? '/gta-optimiser' : '', // Replace 'gta-optimiser' with your repository name
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/gta-optimiser/' : '', // Replace 'gta-optimiser' with your repository name, and include trailing slash
 };
 
 export default nextConfig;
